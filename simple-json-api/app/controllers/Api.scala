a81f52d2b6dfad073f056b51b6ca667946bc8a8c
Marcos Savoury <marcossavoury@hotmail.com>
2014-12-08 21:49:06 -0500

    Adding basic project that can read files from filesystem and send over wire

---
+++ simple-json-api/app/controllers/Api.scala
@@ -0,0 +1,24 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+import play.api.Play.current
+
+object Api extends Controller {
+
+  def index = Action {
+    Ok("This is the controller for the api")
+  }
+
+  def json(name:String) = Action {
+	  Ok("The name you provided was " + name)
+	  try {
+		  val json = Play.classloader.getResourceAsStream(name + ".json");
+		  Ok(scala.io.Source.fromInputStream(json).mkString)
+	  }
+	  catch {
+		  case e: Exception => NotFound("Json file not found")
+	  }
+  }
+
+}
